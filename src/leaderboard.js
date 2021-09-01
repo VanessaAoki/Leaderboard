85e3998367cbc4ba16a55a8021a307f5593d34f7
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-08-31 22:26:12 -0300

    Add leaderboard

---
+++ src/leaderboard.js
@@ -0,0 +1,28 @@
+const myLeaderboard = [
+  {
+    name: "Name",
+    score: 100
+  },
+  {
+    name: "Name",
+    score: 45
+  },
+  {
+    name: "Name",
+    score: 125
+  },
+  {
+    name: "Name",
+    score: 90
+  },
+  {
+    name: "Name",
+    score: 50
+  },
+  {
+    name: "Name",
+    score: 20
+  }
+];
+
+export { myLeaderboard };
---
+++ src/ui.js
@@ -0,0 +1,14 @@
+import { myLeaderboard } from "./leaderboard";
+
+const renderLeaderboard = () => {
+  const list = document.getElementById('leaderboard-list');
+  list.innerHTML = '';
+  myLeaderboard.forEach((score) => {
+    const liScore = document.createElement('li');
+    liScore.classList.add('score-li');
+    liScore.innerHTML = `${score.name}: ${score.score}`;
+    list.appendChild(liScore);
+  });
+};
+
+export { renderLeaderboard };
